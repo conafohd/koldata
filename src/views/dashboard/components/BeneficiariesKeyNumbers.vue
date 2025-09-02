fd756cced6a639e3b983af374ba5de40d4194a4e
Olivier Ribiere <o_ribiere@cartong.org>
2025-09-02 23:41:07 +0200

    chore(dashboard): Review beneficiaries number component

--- src/views/dashboard/components/BeneficiariesKeyNumbers.vue
+++ src/views/dashboard/components/BeneficiariesKeyNumbers.vue
@@ -1,29 +1,43 @@
 <template>
   <div class="BeneficiariesNumbers">
-    <div class="d-flex flex-column align-center">
-      <span class="BeneficiariesNumbers__label">{{
-        $t('dashboard.beneficiariesCount.target')
-      }}</span>
-      <span class="BeneficiariesNumbers__value BeneficiariesNumbers__value--target">{{
-        formatNumber(dashboardStore.stats?.beneficiaries_count.target)
-      }}</span>
-    </div>
+    <span class="BeneficiariesNumbers__title">{{
+      $t('dashboard.beneficiariesCount.beneficiaries')
+    }}</span>
+    <span class="BeneficiariesNumbers__percentage">{{ percentage + '%' }}</span>
 
-    <div class="d-flex flex-column align-center">
-      <span class="BeneficiariesNumbers__label">{{
-        $t('dashboard.beneficiariesCount.beneficiaries')
-      }}</span>
-      <span class="BeneficiariesNumbers__value BeneficiariesNumbers__value--beneficiaries">{{
+    <v-progress-linear
+      :model-value="percentage"
+      height="25"
+      rounded
+      color="light-blue"
+      striped
+    ></v-progress-linear>
+    <div class="BeneficiariesNumbers__details">
+      <span class="BeneficiariesNumbers__details--beneficiariesCount">{{
         formatNumber(dashboardStore.stats?.beneficiaries_count.beneficiaries)
       }}</span>
+      <div>
+        <span>{{ $t('dashboard.beneficiariesCount.objective') }}: </span>
+        <span class="BeneficiariesNumbers__details--objectiveCount">{{
+          formatNumber(dashboardStore.stats?.beneficiaries_count.target)
+        }}</span>
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { formatNumber } from '@/services/utils/FormatNumber'
 import { useDashboardStore } from '@/stores/dashboardStore'
+import { computed } from 'vue'
 
 const dashboardStore = useDashboardStore()
+
+const percentage = computed(() =>
+  (
+    ((dashboardStore.stats?.beneficiaries_count.beneficiaries as number) * 100) /
+    (dashboardStore.stats?.beneficiaries_count.target as number)
+  ).toFixed(2),
+)
 </script>
 <style lang="scss">
 .BeneficiariesNumbers {
@@ -31,19 +45,33 @@ const dashboardStore = useDashboardStore()
   flex-direction: column;
   flex-grow: 1;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
+  gap: 1rem;
+  margin: 1rem 3rem;
+  font-size: 1.2rem;
 }
-.BeneficiariesNumbers__label {
-  font-weight: 600;
-}
-.BeneficiariesNumbers__value {
-  font-size: 3rem;
+.BeneficiariesNumbers__title {
   font-weight: bolder;
-  &--target {
-    color: rgb(var(--v-theme-light-blue));
+}
+.BeneficiariesNumbers__percentage {
+  font-size: 2rem;
+  font-weight: bolder;
+}
+
+.BeneficiariesNumbers__details {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+
+  &--beneficiariesCount {
+    font-size: 1.6rem;
+    font-weight: bolder;
   }
-  &--beneficiaries {
-    color: rgb(var(--v-theme-main-blue));
+
+  &--objectiveCount {
+    font-size: 1.4rem;
+    font-weight: bolder;
   }
 }
 </style>
