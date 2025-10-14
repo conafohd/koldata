be2e0a5be63daee93edaca539e2b82df9894c887
Pierre-Louis Duthoit <pduthoit>
2025-10-14 14:48:41 +0200

    fix: postgis migrations

---
+++ supabase/migrations/20250917144006_postgis_in_extensions_schema.sql
@@ -0,0 +1,78 @@
+-- Drop extension and everything depending on it
+-- DROP EXTENSION postgis CASCADE;
+-- CREATE EXTENSION postgis SCHEMA extensions;
+
+
+DROP EXTENSION postgis CASCADE;
+
+create extension if not exists "postgis" with schema "extensions";
+ALTER TABLE "public"."provinces" DROP COLUMN centroid;
+ALTER TABLE "public"."provinces" ADD centroid::extensions.geometry;
+  
+ALTER TABLE "public"."territoires" DROP COLUMN centroid;
+ALTER TABLE "public"."territoires" ADD centroid::extensions.geometry;
+  
+
+ALTER TABLE "public"."zones_sante" DROP COLUMN centroid;
+ALTER TABLE "public"."zones_sante" ADD centroid::extensions.geometry;
+  
+
+revoke delete on table "public"."spatial_ref_sys" from "anon";
+
+revoke insert on table "public"."spatial_ref_sys" from "anon";
+
+revoke references on table "public"."spatial_ref_sys" from "anon";
+
+revoke select on table "public"."spatial_ref_sys" from "anon";
+
+revoke trigger on table "public"."spatial_ref_sys" from "anon";
+
+revoke truncate on table "public"."spatial_ref_sys" from "anon";
+
+revoke update on table "public"."spatial_ref_sys" from "anon";
+
+revoke delete on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke insert on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke references on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke select on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke trigger on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke truncate on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke update on table "public"."spatial_ref_sys" from "authenticated";
+
+revoke delete on table "public"."spatial_ref_sys" from "postgres";
+
+revoke insert on table "public"."spatial_ref_sys" from "postgres";
+
+revoke references on table "public"."spatial_ref_sys" from "postgres";
+
+revoke select on table "public"."spatial_ref_sys" from "postgres";
+
+revoke trigger on table "public"."spatial_ref_sys" from "postgres";
+
+revoke truncate on table "public"."spatial_ref_sys" from "postgres";
+
+revoke update on table "public"."spatial_ref_sys" from "postgres";
+
+revoke delete on table "public"."spatial_ref_sys" from "service_role";
+
+revoke insert on table "public"."spatial_ref_sys" from "service_role";
+
+revoke references on table "public"."spatial_ref_sys" from "service_role";
+
+revoke select on table "public"."spatial_ref_sys" from "service_role";
+
+revoke trigger on table "public"."spatial_ref_sys" from "service_role";
+
+revoke truncate on table "public"."spatial_ref_sys" from "service_role";
+
+revoke update on table "public"."spatial_ref_sys" from "service_role";
+
+drop type "public"."geometry_dump";
+
+drop type "public"."valid_detail";
